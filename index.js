eaa78e7ad0e51131d4d55f2c6b2f37664b5368f0
Charles Latrobe <Charlie.bateman117@gmail.com>
2021-01-14 09:07:09 -0800

    add index.js and pseudocode.

---
+++ index.js
@@ -0,0 +1,17 @@
+// create question object
+// create prompt handler function and answer handler function
+// include fs to write answers in MD file
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
