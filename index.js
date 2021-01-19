ad90ecb564b353c3a13ec1a85bfec4da6be2f1de
Charles Latrobe <Charlie.bateman117@gmail.com>
2021-01-18 20:36:18 -0800

    add questions for readme generation.

--- index.js
+++ index.js
@@ -1,17 +1,87 @@
 // create question object
 // create prompt handler function and answer handler function
 // include fs to write answers in MD file
+// build (class/constructor? )
 const fs = require('fs');
 const inquirer = require('inquirer');
 
+
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+  type: 'input',
+  name: 'projectTitle',
+  message: 'Enter the name of your project'
+},
+{
+  type: 'input',
+  name: 'description',
+  message: 'Write a description for your application'
+},
+{
+  type: 'input',
+  name: 'installationInstructions',
+  message: 'Enter the instructions for installing your application'
+},
+{
+  type: 'input',
+  name: 'usage',
+  message: 'Enter usage information for your application'
+},
+{
+  type: 'input',
+  name: 'contribution',
+  message: 'Enter guidlines for contributing to your application'
+},
+{
+  type: 'input',
+  name: 'test',
+  message: 'Enter test information for your application'
+},
+]
+
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+//function writeToFile(answers) 
 
 // TODO: Create a function to initialize app
-function init() {}
+//function init() {}
 
 // Function call to initialize app
-init();
+//init();
+
+//# README Generator
+
+//## Description
+
+//## Table of Contents 
+
+//* [Installation](#installation)
+
+//* [Usage](#usage)
+
+//* [Contributing](#contributing)
+
+//* [Tests](#tests)
+
+//* [Questions](#questions)
+
+//## Installation
+
+//## Usage
+  
+//## Contributing
+
+//## Tests
+
+//To run tests, run the following command:
+
+//## Questions
+
+
+inquirer
+  .prompt(questions)
+  .then(answers => {
+  console.log(answers);
+
+});
